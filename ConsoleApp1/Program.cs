b3a5fcb5ab7444436520f35b22dfc120be7020fd
BearForesterSmallBeef <92844590+BearForesterSmallBeef@users.noreply.github.com>
2022-03-26 13:00:36 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ch = true; int n = 1;
+            do
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                if (Math.Abs(n) % 2 == 1) ch = !ch;
+            } while (n != 0);
+            if (ch) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+    }
+}
