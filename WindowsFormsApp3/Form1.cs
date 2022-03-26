545d9a6f51226b46ae7cbf3c18d59ff99caf68e0
BearForesterSmallBeef <92844590+BearForesterSmallBeef@users.noreply.github.com>
2022-03-26 12:59:41 +0300

    Add files via upload

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            button1.BackColor = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+            button1.Location = new Point(rnd.Next(0, this.Width - button1.Width - 5), rnd.Next(0, this.Height - button1.Height - 5));
+        }
+    }
+}
